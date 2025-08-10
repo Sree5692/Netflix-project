1ceca53881ceba4946ec8138489dae50d3fddd50
Ubuntu <ubuntu@ip-172-31-92-55.ec2.internal>
2025-08-10 14:47:41 +0000

    edited Jenkins cicd-install.sh

--- Jenkins-TF/cicd-install.sh
+++ Jenkins-TF/cicd-install.sh
@@ -31,6 +31,30 @@ sudo chmod 777 /var/run/docker.sock
 #!/bin/bash
 docker run -d  --name sonar -p 9000:9000 sonarqube:lts-community
 
+
+# Installing AWS CLI
+#!/bin/bash
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip -y
+unzip awscliv2.zip
+sudo ./aws/install
+
+# Installing Kubectl
+#!/bin/bash
+sudo apt update
+sudo apt install curl -y
+sudo curl -LO "https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
+sudo chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+kubectl version --client
+
+# Installing Terraform
+#!/bin/bash
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt install terraform -y
+
 # Installing Trivy
 #!/bin/bash
 sudo apt-get install wget apt-transport-https gnupg lsb-release -y
@@ -47,3 +71,6 @@ wget https://github.com/jeremylong/DependencyCheck/releases/download/v8.4.0/depe
 unzip dependency-check-8.4.0-release.zip
 path taken
 /opt/owasp-dc/dependency-check
+sudo chown -R jenkins:jenkins /opt/owasp-dc/dependency-check/data
+sudo chmod -R 755 /opt/owasp-dc/dependency-check/data
+sudo chmod -R 777 /opt/owasp-dc/dependency-check/data
