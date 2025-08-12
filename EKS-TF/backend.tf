5c4e5f6c0b4bf5b8b20d0ae00d11eeca041fac07
Sree5692 <136165217+Sree5692@users.noreply.github.com>
2025-08-12 17:47:20 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-reddit-bucket-2025"
     region         = "us-east-1"
-    key            = "EKS-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
+    key            = "EKS-DevSecOps-Project/EKS-TF/terraform.tfstate"
    # dynamodb_table = "Lock-Files"
     use_lockfile = false
     encrypt        = true
